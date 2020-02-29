b3a7c4ab4aefbcd7b1c8446541bc1867c26d3822
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-29 22:46:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -60,7 +60,7 @@ end
   
    if @sides[0] == @sides[1] && @sides[0] == @sides[2] && @sides[1] == @sides[2]
      result = 3
-   elsif @sides[0] == @sides[1] || @sides[0] == @sides[3] || @sides[1] == @sides[2] || @sides[2] == @sides[3]
+   elsif @sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2] 
    result = 2
  else
    result = 1
