16cea33454a22d36a57ef5ed9819620029338264
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-29 20:48:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,49 @@
+require 'pry'
+class TriangleError < StandardError; end
 
-class TriangleError < StandardError
-end
-
+# class to determine Triangle type
 class Triangle
-  def initialize(s1, s2, s3)
-    @sides = [s1, s2, s3]
-
-    if @sides.any? {|side| side <= 0}
-      raise TriangleError.new, "Cannot create a triangle "
-    end
-
-    @sides.combination(2).each do |combination|
-      last_side = @sides.clone
-      combination.each {|item| last_side.delete(item)}
-      if !last_side.empty? && combination.inject(:+) <= last_side.first
-        raise TriangleError.new, "Cannot create a triangle"
-      end
-    end
-
-    classify
-  end
-
-  def classify
-    if @sides.uniq.count == 1
-      @kind = :equilateral
-    elsif @sides.uniq.count == 2
-      @kind = :isosceles
-    else
-      @kind = :scalene
-    end
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+    validate
   end
 
   def kind
-    @kind
+    equilateral? || isosceles? || scalene?
+  end
+
+  private
+
+  def validate
+    no_sides? || negative_sides? || triangle_inequality?
+  end
+
+  def no_sides?
+    fail TriangleError, 'sides have no length' if @sides.find { |n| n = 0 
+  end
+
+  def negative_sides?
+    fail TriangleError, 'negative side(s)' if @sides.find { |n| n < 0 }
+  end
+
+  def triangle_inequality?
+    sorted = @sides.sort
+    fail TriangleError, 'inequality' if sorted[0] + sorted[1] <= sorted[2]
+  end
+
+  def equilateral?
+    :equilateral if unique_sides == 1
+  end
+
+  def isosceles?
+    :isosceles if unique_sides == 2
+  end
+
+  def scalene?
+    :scalene if unique_sides == 3
+  end
+
+  def unique_sides
+    @sides.uniq.size
   end
 end
